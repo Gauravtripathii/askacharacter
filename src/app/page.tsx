5e4dbdccedc45fa24fbcb31a325a798659e1fcb0
gauravtripathii <gauravtripathii>
2024-08-18 01:23:36 +0530

    responsive initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
       .catch((error: AxiosError) => console.log(error));
   };
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-10 p-10">
-      <div className="flex gap-5">
+    <main className="w-screen h-screen flex flex-col items-center justify-center gap-5 lg:gap-10 lg:p-10">
+      <div className="flex flex-col lg:flex-row gap-2 lg:gap-5">
         <input type="text" placeholder="What would you like to ask the toddler?" onInput={event => setQuestion((event.target as HTMLInputElement).value)} className="border px-2 py-3 rounded-xl w-[400px]" />
         <button onClick={() => handleSubmit(question)} className="border border-black px-5 py-3 rounded-xl font-bold bg-black hover:bg-white text-white hover:text-black">ask</button>
       </div>
-      <div className="border p-5 rounded-2xl w-1/2">{answer}</div>
+      <div className="border p-5 rounded-2xl lg:w-1/2">{answer}</div>
     </main>
   );
 }
