ca8ac68fc8759315c9c068713a86c90b933fd56c
gauravtripathii <gauravtripathii>
2024-08-18 11:44:14 +0530

    mobile version complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,63 @@
 import { useState } from "react";
 import axios, { AxiosError } from "axios";
 
+interface charactersType {
+  [key: string]: string;
+}
+
+const characters: charactersType = {
+  toddler:
+    "like a toddler in a playful and imaginative way (don't mention the toddler thing)",
+  deadpool:
+    "like deadpool with a touch of humor (mention the deadpool, just answer)",
+};
+const charNames: string[] = ["toddler", "deadpool"];
+
 export default function Home() {
   const [question, setQuestion] = useState<string>("");
   const [answer, setAnswer] = useState<string>("");
 
+  const [character, setCharacter] = useState("");
+
   const handleSubmit = async (question: string) => {
-    const questionModified = 
-    `Answer '${question}' like me and you are a toddler. (Just give answer please and do not mention the toddler thing and make it silly)`
-    await axios
-      .post("/api/getAnswer", { question: questionModified })
-      .then((res) => {
-        setAnswer(res.data);
-      })
-      .catch((error: AxiosError) => console.log(error));
+    const questionModified = `Answer '${question}' ${character} (do not at all mention what the prompt is)`;
+    if (question) {
+      await axios
+        .post("/api/getAnswer", { question: questionModified })
+        .then((res) => {
+          setAnswer(res.data);
+        })
+        .catch((error: AxiosError) => console.log(error));
+    }
   };
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-5 lg:gap-10 lg:p-10">
-      <div className="flex flex-col lg:flex-row gap-2 lg:gap-5">
-        <input type="text" placeholder="What would you like to ask the toddler?" onInput={event => setQuestion((event.target as HTMLInputElement).value)} className="border px-2 py-3 rounded-xl w-[400px]" />
-        <button onClick={() => handleSubmit(question)} className="border border-black px-5 py-3 rounded-xl font-bold bg-black hover:bg-white text-white hover:text-black">ask</button>
+    <main className="w-screen h-screen flex flex-col items-center justify-center p-5 gap-5">
+      <div className="bg-red-500 w-full h-1/12 p-5 text-xl text-white font-bold rounded-2xl flex items-center gap-5">
+        {charNames.map((char, index) => (
+          <div key={index} onClick={() => setCharacter(characters[char])} className={`hover:cursor-pointer hover:underline`}>
+            {char.toUpperCase()}
+          </div>
+        ))}
       </div>
-      <div className="border p-5 rounded-2xl lg:w-1/2">{answer}</div>
+      <div className="w-full h-full flex flex-col gap-5">
+        <div className="border rounded-2xl w-full h-full p-5">{answer}</div>
+        <div className="w-full flex flex-col gap-2">
+          <input
+            type="text"
+            placeholder="What would you like to ask?"
+            onInput={(event) =>
+              setQuestion((event.target as HTMLInputElement).value)
+            }
+            className="border border-black px-3 py-3 rounded-xl"
+          />
+          <button
+            onClick={() => handleSubmit(question)}
+            className="border border-black px-5 py-3 rounded-xl font-bold bg-black hover:bg-white text-white hover:text-black"
+          >
+            ask
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
